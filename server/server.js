c1b24af320bde87dca04d106d3690b0c9bf80fbb
leonetti <alexleonetti.dev@gmail.com>
2022-01-02 14:56:22 -0800

    Express RESTful API connect to PostgreSQL
    CRUD routes each connected to PostgreSQL DB table - restaurants
    
    GET    ALL restaurants
    GET    ONE restaurant
    CREATE ONE restaurant
    UPDATE ONE restaurant
    DELETE ONE restaurant
    
    To Do:
    Error handling
    Different types of success

--- server/server.js
+++ server/server.js
@@ -18,52 +18,102 @@ app.use(morgan('dev'));
 
 // GET ALL restaurants
 app.get('/api/v1/restaurants', async (req, res) => {
-  const results = await db.query('SELECT * FROM restaurants');
-  console.log(results);
+  try {
+    const results = await db.query(
+      'SELECT * FROM restaurants',
+    );
 
-  res.status(200).json({
-    status: 'success',
-    data: {
-      restaurants: ['mcdonalds', 'wendys'],
-    },
-  });
+    res.status(200).json({
+      status: 'success',
+      length: results.rows.length || 0,
+      data: {
+        restaurants: results.rows,
+      },
+    });
+  } catch (error) {
+    // TO DO
+    console.log(error);
+  }
 });
 
 // GET ONE restaurant
-app.get('/api/v1/restaurants/:id', (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      restaurant: 'mcdonalds',
-    },
-  });
+app.get('/api/v1/restaurants/:id', async (req, res) => {
+  try {
+    const results = await db.query(
+      'SELECT * FROM restaurants WHERE id = $1',
+      [req.params.id],
+    );
+
+    res.status(200).json({
+      status: 'success',
+      length: results.rows.length || 0,
+      data: {
+        restaurant: results.rows[0],
+      },
+    });
+  } catch (error) {
+    // TO DO
+    console.log(error);
+  }
 });
 
 // CREATE ONE restaurant
-app.post('/api/v1/restaurants', (req, res) => {
-  res.status(201).json({
-    status: 'success',
-    data: {
-      restaurant: 'mcdonalds',
-    },
-  });
+app.post('/api/v1/restaurants', async (req, res) => {
+  try {
+    const results = await db.query(
+      'INSERT INTO restaurants (name, location, price_range) VALUES ($1, $2, $3) returning *',
+      [req.body.name, req.body.location, req.body.price_range],
+    );
+
+    res.status(201).json({
+      status: 'success',
+      length: results.rows.length || 0,
+      data: {
+        restaurant: results.rows[0],
+      },
+    });
+  } catch (error) {
+    // TO DO
+    console.log(error);
+  }
 });
 
 // UPDATE ONE restaurant
-app.put('/api/v1/restaurants/:id', (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      restaurant: 'mcdonalds',
-    },
-  });
+app.put('/api/v1/restaurants/:id', async (req, res) => {
+  try {
+    const results = await db.query(
+      'UPDATE restaurants SET name = $1, location = $2, price_range = $3 WHERE id = $4 returning *',
+      [req.body.name, req.body.location, req.body.price_range, req.params.id],
+    );
+
+    res.status(200).json({
+      status: 'success',
+      length: results.rows.length || 0,
+      data: {
+        restaurant: results.rows[0],
+      },
+    });
+  } catch (error) {
+    // TO DO
+    console.log(error);
+  }
 });
 
 // DELETE ONE restaurant
 app.delete('/api/v1/restaraunts/:id', (req, res) => {
-  res.status(204).json({
-    status: 'success',
-  });
+  try {
+    db.query(
+      'DELETE FROM restaurants WHERE id = $1',
+      [req.params.id],
+    );
+
+    res.status(204).json({
+      status: 'success',
+    });
+  } catch (error) {
+    // TO DO
+    console.log(error);
+  }
 });
 
 // binds and listens for connections on the specified host and port
